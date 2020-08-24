54a17841cbf8eb0e059ce0d70fa17797bde2ccb3
SangMin-Code <ragdexplorer@gmail.com>
2020-08-24 21:55:29 +0900

    등록 API

---
+++ src/main/java/com/test/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.test.book.springboot.service.posts;
+
+import com.test.book.springboot.domain.posts.PostsRepository;
+import com.test.book.springboot.web.Dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
