a6e7b55789733b65e3c09138b464a3181bcc9b24
SangMin-Code <ragdexplorer@gmail.com>
2020-08-25 22:13:31 +0900

    수정/조회

--- src/main/java/com/test/book/springboot/web/PostsApiController.java
+++ src/main/java/com/test/book/springboot/web/PostsApiController.java
@@ -1,11 +1,10 @@
 package com.test.book.springboot.web;
 import com.test.book.springboot.service.posts.PostsService;
+import com.test.book.springboot.web.Dto.PostsResponseDto;
 import com.test.book.springboot.web.Dto.PostsSaveRequestDto;
+import com.test.book.springboot.web.Dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,4 +17,13 @@ public class PostsApiController {
         return postsService.save(requestDto);
     }
 
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id,requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
 }
